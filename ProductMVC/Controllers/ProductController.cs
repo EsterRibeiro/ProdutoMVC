89759854af8e347dddb74fd1208dfa6894f3b80a
Ester Santos <ester.santos@teaditazure.onmicrosoft.com>
2020-01-28 16:53:42 -0300

    Atualização do .net core 2.2 > 3.1. Inclusão de delete e create

---
+++ ProductMVC/Controllers/ProductController.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ProductMVC.Models;
+using ProductMVC.Repository;
+
+namespace ProductMVC.Controllers
+{
+    public class ProductController : Controller
+    {
+
+        ProductRepository productRepository = new ProductRepository();
+        // GET: Product
+        public ActionResult Index()
+        {
+            var usuarios = productRepository.GetProductsList();
+            return View(usuarios);
+        }
+
+        // GET: Product/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Product/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Product/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind] Product produto)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+                if (ModelState.IsValid)
+                {
+                    productRepository.CreateProduct(produto);
+                    return RedirectToAction(nameof(Index));
+                }
+
+                return View(produto);
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Product/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Product/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Product/Delete/5
+        public ActionResult Delete([Bind] Product product)
+        {
+            productRepository.DeleteProduct(product);
+            return RedirectToAction(nameof(Index));
+        }
+
+        // POST: Product/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ ProductMVC/Repository/ProductRepository.cs
@@ -0,0 +1,158 @@
+﻿using ProductMVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ProductMVC.Repository
+{
+    public class ProductRepository
+    {
+        //string de conexão
+        string stringconn = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=Produto;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+
+        public IEnumerable<Product> GetProductsList()
+        {
+            //instância dos produtos
+            List<Product> prods = new List<Product>();
+
+            //usando a string de conexão para os seguintes procedimentos
+            using (var connection = new SqlConnection(stringconn))
+            {
+                //query
+                var commandText = "select * from Produtos";
+
+                //ligando o comando da query a conexão
+                SqlCommand selectCommand = new SqlCommand(commandText, connection);
+
+                try
+                {
+                    //abrindo a conexão
+                    connection.Open();
+
+                    //lendo todas as informações do banco 
+                    using (var reader = selectCommand.ExecuteReader())
+                    {
+                        //enquanto tiver 
+                        while (reader.Read())
+                        {
+                            var produto = new Product();
+
+                            produto.Id = (int)reader["Id"];
+                            produto.Nome = reader["Nome_Produto"].ToString();
+                            produto.Fabricante = reader["Fabricante"].ToString();
+                            produto.CodigoBarras = reader["CodigoBarras"].ToString();
+                            produto.Preco = (decimal)reader["Preco"];
+                            produto.Estoque = (int)reader["Estoque"];
+
+                            //adicionando a lista do banco na variável do tipo lista
+                            prods.Add(produto);
+                        }
+
+                    }
+
+                }
+                catch (Exception ex)
+                {
+                    throw ex;
+                }
+                finally
+                {
+                    connection.Close();
+                }
+
+            }
+
+            return prods;
+
+        }
+
+
+        public void CreateProduct(Product prod)
+        {
+
+           
+            //usando a string de conexão
+            using (var connection = new SqlConnection(stringconn))
+            {
+                //nonquery
+                var query = "insert into Produtos(Nome_Produto, Fabricante, CodigoBarras, Preco, Estoque)" +
+                    " values(@Nome_Produto, @Fabricante, @CodigoBarras, @Preco, @Estoque)";
+
+                //ligando a conexão com o comando
+                SqlCommand command = new SqlCommand(query, connection);
+
+                //adicionando os novos valores ao banco
+                command.CommandType = new CommandType();
+
+                command.Parameters.AddWithValue("@Nome_Produto", prod.Nome);
+                command.Parameters.AddWithValue("@Fabricante", prod.Fabricante);
+                command.Parameters.AddWithValue("@CodigoBarras", prod.CodigoBarras);
+                command.Parameters.AddWithValue("@Preco", prod.Preco);
+                command.Parameters.AddWithValue("@Estoque", prod.Estoque);
+
+                
+
+                try
+                {
+                    //abre a conexão e executa
+                    connection.Open();
+                    command.ExecuteNonQuery();
+
+
+                }catch(Exception ex)
+                {
+                    throw ex;
+                }
+                finally
+                {
+                    //fecha a conexão
+                    connection.Close();
+                }
+
+
+            }
+
+
+        }
+
+        public void UpdateProduct(int id) { }
+
+        public void DeleteProduct(Product prod) 
+        {
+            using (var connection = new SqlConnection(stringconn))
+            {
+                string query = "DELETE FROM Produtos WHERE Id = @Id";
+
+                SqlCommand command = new SqlCommand(query, connection);
+
+                command.Parameters.AddWithValue("@Id", prod.Id);
+
+                try
+                {
+                    connection.Open();
+                    command.ExecuteNonQuery();
+
+                }catch(Exception ex)
+                {
+                    throw ex;
+
+                }
+                finally
+                {
+                    connection.Close();
+                }
+
+
+
+            }
+        }
+
+        public void GetProductById(int id) { }
+    }
+
+
+}
+
